344bd013682087bb51e0843ffa98ab714b35fa4b
Uluru92 <jorddy.castro@gmail.com>
2024-04-25 20:43:56 -0600

    PRACTICA 1

---
+++ Practica1.sql
@@ -0,0 +1,8 @@
+-- ESTO ES UN COMENTARIO
+
+-- Ejercicio Básico: Empleados y Contactos 
+--Objetivo: Crear una consulta SQL que liste 
+--los nombres, apellidos, títulos de trabajo y 
+--direcciones de correo electrónico de los empleados 
+--cuyos apellidos comiencen con la letra 'A'. Los resultados 
+--deben ordenarse por apellido.
