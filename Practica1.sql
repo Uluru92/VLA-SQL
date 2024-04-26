7e4b004e1fe092e7d96e996b6a386aa8d671fc9b
Uluru92 <jorddy.castro@gmail.com>
2024-04-25 21:05:45 -0600

    ejercicio 1

--- Practica1.sql
+++ Practica1.sql
@@ -6,3 +6,22 @@
 --direcciones de correo electrónico de los empleados 
 --cuyos apellidos comiencen con la letra 'A'. Los resultados 
 --deben ordenarse por apellido.
+
+--SELECT LN.EmailAddress, LN.FirstName, LN.LastName, LN.JobTitle
+--FROM HumanResources.vEmployee AS LN
+--WHERE LastName LIKE	'A%'
+--order by ln.LastName
+
+
+select 
+	p.FirstName, 
+	p.LastName,
+	e.JobTitle,
+	ea.EmailAddress
+from Person.Person as p
+join HumanResources.Employee as e
+	on p.BusinessEntityID = e.BusinessEntityID
+join Person.EmailAddress as ea
+	on p.BusinessEntityID = ea.BusinessEntityID
+where p.LastName like 'A%'
+order by p.LastName
